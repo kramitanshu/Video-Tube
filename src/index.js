e145247270a0a079e9c52d893ada0ec195844657
Kumar Amitanshu Singh <kumaramitanshu2@gmail.com>
2024-03-09 08:21:00 +0530

    Utils code, Handling Error and Success standard message

--- src/index.js
+++ src/index.js
@@ -1,13 +1,53 @@
-// require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
-
-// import mongoose from "mongoose";
-// import { DB_NAME } from "./constants";
 import connectDB from "./db/index.js";
+
  
 
 dotenv.config({path: './env'})
-connectDB()
+
+connectDB().then(()=> {
+
+  // checking event for error
+  app.on("error", (error) => {
+    console.log("ERR: ", error);
+    throw error
+  })
+
+  app.listen(process.env.PORT || 8000, ()=>{
+    console.log(`Server is running at port: ${process.env.PORT}`)
+  })
+}).catch((error) => {
+  console.log("MONGO db connection failed !!!", error);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
